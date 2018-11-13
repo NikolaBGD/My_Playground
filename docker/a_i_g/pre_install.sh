0a3719c5f052c89dd34c9eea23e01434b08e4c6a
Robin B Yang (RD-TW-TMP) <robin_b_yang@trendmicro.com>
2018-11-13 17:53:17 +0800

    clone docker: influxdb + grafana + telegraf

---
+++ docker/a_i_g/pre_install.sh
@@ -0,0 +1,4 @@
+docker pull alpine:3.8
+docker pull influxdb:1.6.4-alpine
+docker pull telegraf:1.6.4-alpine
+docker pull grafana/grafana
---
+++ docker/a_n_m_u_d/pre_install.sh
@@ -0,0 +1,4 @@
+docker pull alpine:3.8
+docker pull nginx:1.15.6-alpine
+docker pull mysql:8.0
+docker pull django
