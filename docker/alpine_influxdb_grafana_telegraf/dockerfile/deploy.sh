be0f6432bd96a495f24d0c6777336ddc47bd64c2
Robin B Yang (RD-TW-TMP) <robin_b_yang@trendmicro.com>
2018-12-12 17:59:25 +0800

    Add script deploying grafana, influxdb, and telegraf by using docker.io

--- docker/alpine_influxdb_grafana_telegraf/dockerfile/deploy.sh
+++ docker/alpine_influxdb_grafana_telegraf/dockerfile/deploy.sh
@@ -1,12 +1,31 @@
+# grafana
+docker pull grafana/grafana
+docker run \ 
+    -d --name=grafana \ 
+    -p 3000:3000 grafana/grafana \ 
+    -v $PWD/grafana.ini:/etc/grafana/grafana.ini \ 
+    -e "GF_INSTALL_PLUGINS=grafana-clock-panel" \ 
+    -e "GF_INSTALL_PLUGINS=grafana-piechart-panel" \
+    -e "GF_INSTALL_PLUGINS=grafana-worldmap-panel" \
+    -e "GF_INSTALL_PLUGINS=goshposh-metaqueries-datasource" \
+    -v $PWD:/var/lin/grafana
+    grafana/grafana --config=/etc/grafana/grafana.ini
+
 # influxdb
-influxd -config /etc/influxdb/influxdb.conf
-service start influxdb
+docker pull influxdb:1.7.1
+docker run \ 
+    -d --name=influxdb \ 
+    -p 8086:8086 \ 
+    -v $PWD:/var/lib/influxdb \ 
+    -v $PWD/influxdb.conf:/etc/influxdb/influxdb.conf:ro \ 
+    influxdb:1.7.1 -config /etc/influxdb/influxdb.conf
+
 
 # telegraf
-service start telegraf
-
-# grafana
-plugins = cat grafana_plugins
-grafana-cli plugins install $plugins
-service start grafana
+docker pull telegraf:1.9.0
+docker run \ 
+    -d --name=telegraf \
+    --net=influxdb \
+    -v $PWD/telegraf.conf:/etc/telegraf/telegraf.conf:ro \ 
+    telegraf:1.9.0 
 
